b857736bbefa051af8667d4252ec999fb4bb049c
MD. MOINUL Hossian <mdmoinulhossain100@gmail.com>
2023-12-14 21:43:11 +0600

    prevention on multy row insert in a database table

--- save-text.php
+++ save-text.php
@@ -1,9 +1,20 @@
 <?php
 include './db.php';
 
+
+// Check if there's already a row in the table
+$result = $conn->query("SELECT COUNT(*) AS total FROM `text_content`");
+if ($result) {
+    $row = $result->fetch_assoc();
+    if ($row['total'] > 0) {
+        echo "One row added in the database. You cannot add another one.";
+        exit(); // Stop further execution
+    }
+}
+
+
 // Get text from the editor
 $content = $_POST['editor_content'];
-
 // Insert the text into the database (using prepared statement to prevent SQL injection)
 $stmt = $conn->prepare("INSERT INTO `text_content`(`content`) VALUES (?)");
 $stmt->bind_param("s", $content);
